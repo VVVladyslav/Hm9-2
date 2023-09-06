19c7e135d1a724b4197809e493acc32ed2b69f46
VVVladyslav <vladoszhar@gmail.com>
2023-09-06 15:18:00 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        MyLinkedList linkedList = new MyLinkedList();
+        linkedList.add("hello");
+        linkedList.remove(0);
+        linkedList.clear();
+        linkedList.add("hello");
+        linkedList.size();
+        linkedList.get(0);
+    }
+}
---
+++ src/MyLinkedList.java
@@ -0,0 +1,76 @@
+public class MyLinkedList {
+    private Node head;
+    private Node tail;
+    private int size;
+
+    private class Node {
+        Object data;
+        Node prev;
+        Node next;
+
+        Node(Object data) {
+            this.data = data;
+        }
+    }
+
+    public void add(Object value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+        } else {
+            newNode.prev = tail;
+            tail.next = newNode;
+        }
+        tail = newNode;
+        size++;
+    }
+
+
+    public void remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+
+        if (current.prev == null) {
+            head = current.next;
+        } else {
+            current.prev.next = current.next;
+        }
+
+        if (current.next == null) {
+            tail = current.prev;
+        } else {
+            current.next.prev = current.prev;
+        }
+
+        size--;
+    }
+
+    public void clear() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public Object get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+
+        return current.data;
+    }
+}
